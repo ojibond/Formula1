a26433caf525b8ced6813448137da089518919d5
Bond Oji <ojibond150@gmail.com>
2023-09-09 23:09:22 -0500

    added classes to be mapped using code-first ef approach.

---
+++ FormulaOne.Entities/DbSet/Driver.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormulaOne.Entities.DbSet
+{
+    public class Driver : BaseEntity
+    {
+        public Driver()
+        {
+            Achievments = new HashSet<Achievment>();
+        }
+        public string FirstName { get; set; } = string.Empty;
+        public string Lastname { get; set; } = string.Empty;
+        public int DriverNumber { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+        //navigation property
+        public virtual ICollection<Achievment> Achievments { get; set; }
+    }
+}
