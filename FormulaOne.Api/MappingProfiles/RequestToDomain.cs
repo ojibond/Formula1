b57d447a8b10459c7acb1f13b8aab479a2c90108
Bond Oji <ojibond150@gmail.com>
2023-09-21 01:43:44 -0500

    added automappers to mapp request and response to Dtos.

---
+++ FormulaOne.Api/MappingProfiles/RequestToDomain.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using FormulaOne.Entities.DbSet;
+using FormulaOne.Entities.Dtos.Requests;
+
+namespace FormulaOne.Api.MappingProfiles
+{
+    public class RequestToDomain : Profile
+    {
+        public RequestToDomain()
+        {
+            CreateMap<CreateDriverAchievmentRequest, Achievment>()
+                .ForMember(
+                    dest => dest.RaceWins,
+                    opt => opt.MapFrom(src => src.Wins))
+                .ForMember(
+                    dest => dest.Status,
+                    opt => opt.MapFrom(src => 1))
+                .ForMember(
+                    dest => dest.AddedDate,
+                    opt => opt.MapFrom(src => DateTime.UtcNow))
+                .ForMember(
+                    dest => dest.UpdateDate,
+                    opt => opt.MapFrom(src => DateTime.UtcNow));
+
+            CreateMap<UpdateDriverAchievmentRequest, Achievment>()
+                .ForMember(
+                    dest => dest.RaceWins,
+                    opt => opt.MapFrom(src => src.Wins))
+                .ForMember(
+                    dest => dest.UpdateDate,
+                    opt => opt.MapFrom(src => DateTime.UtcNow));
+        }
+    }
+}
--- FormulaOne.Api/Program.cs
+++ FormulaOne.Api/Program.cs
@@ -16,6 +16,8 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
 var app = builder.Build();
