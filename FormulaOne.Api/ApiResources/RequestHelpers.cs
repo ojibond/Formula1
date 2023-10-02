d4227f97e9323f363f3bf3b51af47cc11cb9db8a
Bond Oji <ojibond150@gmail.com>
2023-10-01 21:09:20 -0500

    added user-defined exceptions handling to base class and implemented exceptions to Driver and Achievement class.

---
+++ FormulaOne.Api/ApiResources/RequestHelpers.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace FormulaOne.Api.ApiResources;
+
+public static class RequestHelpers
+{
+    public static HttpResponseMessage WithReasonPhrase(this HttpResponseMessage responseMessage, string reasonPhrase)
+    {
+        responseMessage.ReasonPhrase = reasonPhrase;
+        return responseMessage;
+    }
+}
--- FormulaOne.Api/Controllers/AchievementsController.cs
+++ FormulaOne.Api/Controllers/AchievementsController.cs
@@ -4,7 +4,6 @@ using FormulaOne.Entities.DbSet;
 using FormulaOne.Entities.Dtos.Requests;
 using FormulaOne.Entities.Dtos.Responses;
 using Microsoft.AspNetCore.Mvc;
-using System.Security.Cryptography.X509Certificates;
 
 namespace FormulaOne.Api.Controllers
 {
@@ -17,43 +16,64 @@ namespace FormulaOne.Api.Controllers
         [Route("{driverId:Guid}")]
         public async Task<IActionResult> GetDriverAchievements(Guid driverId)
         {
-            var driverAchievements = await _unitOfWork.Achievements.GetDriverAchievmentAsync(driverId);
+            try
+            {
+                var driverAchievements = await _unitOfWork.Achievements.GetDriverAchievmentAsync(driverId);
 
-            if (driverAchievements == null)
-                return NotFound("Achievements not found");
+                if (driverAchievements == null)
+                    return ReturnNotFoundResultStatus("Driver's achievements not found");
 
-            //map driverAchievements to driverAchievementRresponse.
-            var result = _mapper.Map<DriverAchievementResponse>(driverAchievements);
+                //map driverAchievements to driverAchievementRresponse.
+                var result = _mapper.Map<DriverAchievementResponse>(driverAchievements);
 
-            return Ok(result);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return ReturnRestfulResultForThrownException(ex, new {driverId});
+            }           
         }
 
         [HttpPost("")]
         public async Task<IActionResult> AddAchievement([FromBody] CreateDriverAchievmentRequest achievement)
         {
-            if (!ModelState.IsValid)
-                return BadRequest();
+            try
+            {
+                if (!ModelState.IsValid)
+                    return ReturnBadRequestResultStatus();
 
-            var result = _mapper.Map<Achievement>(achievement);
+                var result = _mapper.Map<Achievement>(achievement);
 
-            await _unitOfWork.Achievements.Add(result);
-            await _unitOfWork.CompleteAsync();
+                await _unitOfWork.Achievements.Add(result);
+                await _unitOfWork.CompleteAsync();
 
-            return CreatedAtAction(nameof(GetDriverAchievements), new { driverId = result.DriverId }, result);
+                return CreatedAtAction(nameof(GetDriverAchievements), new { driverId = result.DriverId }, result);
+            }
+            catch (Exception ex)
+            {
+                return ReturnRestfulResultForThrownException(ex);
+            }
         }
 
         [HttpPut("")]
         public async Task<IActionResult> UpdateAchievements([FromBody] UpdateDriverAchievmentRequest achievement)
         {
-            if(!ModelState.IsValid)
-                return BadRequest();
+            try
+            {
+                if(!ModelState.IsValid)
+                    return ReturnBadRequestResultStatus();
 
-            var result = _mapper.Map<Achievement>(achievement);
+                var result = _mapper.Map<Achievement>(achievement);
 
-            await _unitOfWork.Achievements.Update(result);
-            await _unitOfWork.CompleteAsync();
+                await _unitOfWork.Achievements.Update(result);
+                await _unitOfWork.CompleteAsync();
 
-            return NoContent();
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                return ReturnRestfulResultForThrownException(ex);
+            }
         }
     }
 }
